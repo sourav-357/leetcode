e0f2b1eb65bac4ce7725de628e61c8bc309e4236
Sourav Kumar <xsouravkumar357@gmail.com>
2026-07-08 13:55:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count BST nodes that lie in a given range/count-bst-nodes-that-lie-in-a-given-range.java
@@ -0,0 +1,28 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    int getCount(Node root, int l, int h) {
+
+        if (root == null)
+            return 0;
+
+        if (root.data < l)
+            return getCount(root.right, l, h);
+
+        if (root.data > h)
+            return getCount(root.left, l, h);
+
+        return 1 + getCount(root.left, l, h) + getCount(root.right, l, h);
+    }
+}
