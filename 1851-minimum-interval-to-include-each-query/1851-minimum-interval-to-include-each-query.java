5c9890a53829df8d858feacd499a1fc6e141e2d8
Sourav Kumar <xsouravkumar357@gmail.com>
2026-07-17 19:40:41 +0530

    Time: 102 ms (81.67%), Space: 141.7 MB (45.89%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.java
@@ -1,74 +1,48 @@
-class Interval {
-    int start;
-    int end;
-    int length;
-
-    Interval(int start, int end) {
-        this.start = start;
-        this.end = end;
-        this.length = end - start + 1;
-    }
-}
-
-class Query implements Comparable<Query> {
-    int value;
-    int index;
-
-    Query(int value, int index) {
-        this.value = value;
-        this.index = index;
-    }
-
-    @Override
-    public int compareTo(Query other) {
-        return Integer.compare(this.value, other.value);
-    }
-}
-
 class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
 
-        Interval[] arr = new Interval[intervals.length];
-        Query[] q = new Query[queries.length];
+        int m = intervals.length;
+        int n = queries.length;
 
-        for (int i = 0; i < intervals.length; i++) {
-            arr[i] = new Interval(intervals[i][0], intervals[i][1]);
+        int[][] store = new int[n][2];
+        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
+
+        for(int i=0;i<n;i++){
+            store[i][0] = queries[i];
+            store[i][1] = i;
         }
 
-        for (int i = 0; i < queries.length; i++) {
-            q[i] = new Query(queries[i], i);
-        }
+        Arrays.sort(store,(a,b) -> a[0] - b[0]);
 
-        Arrays.sort(arr, (a, b) -> Integer.compare(a.start, b.start));
-        Arrays.sort(q);
+        int[] ans = new int[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
 
-        PriorityQueue<Interval> pq = new PriorityQueue<>(
-            (a, b) -> {
-                int cmp = Integer.compare(a.length, b.length);
-                if (cmp != 0) return cmp;
-                return Integer.compare(a.end, b.end);
-            }
-        );
+        int i =0;
+        for(int[] q : store) {
+            int val = q[0];
+            int idx = q[1];
 
-        int[] ans = new int[queries.length];
-        int i = 0;
+            while(i < m && intervals[i][0] <= val) {
+                int left = intervals[i][0];
+                int right = intervals[i][1];
+                int len = intervals[i][1] - intervals[i][0] + 1;
 
-        for (Query query : q) {
-
-            // Add all intervals whose start <= query
-            while (i < arr.length && arr[i].start <= query.value) {
-                pq.offer(arr[i]);
+                pq.offer(new int[]{len,right});
                 i++;
             }
 
-            // Remove intervals that cannot contain the query
-            while (!pq.isEmpty() && pq.peek().end < query.value) {
+            while(!pq.isEmpty() && pq.peek()[1] < val) {
                 pq.poll();
             }
 
-            ans[query.index] = pq.isEmpty() ? -1 : pq.peek().length;
-        }
+            if(pq.isEmpty()) {
+                ans[idx] = -1;
+            }
 
+            else{
+                ans[idx] = pq.peek()[0];
+            }
+        }
         return ans;
     }
 }
